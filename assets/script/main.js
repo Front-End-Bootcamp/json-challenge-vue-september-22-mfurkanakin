1ad889657d23ad3f7bc0cc61df28ed983d74d37e
mfurkanakin <mfakin@yahoo.com>
2022-09-15 13:04:58 +0300

    everything requested has been done

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,25 @@
-console.log('👻');
+import data from "../data/data"
+
+
+let getGroup = (param) => {
+	let filteredGroup =  data.filter(obj => obj.group === param)
+	let asistant = filteredGroup.find(obj => obj.type === "assistant")
+	let newGroup = {
+		"group": param,
+		"student": getGroupStudents(filteredGroup) ,
+		"assistant": asistant.name,
+	}
+	return newGroup;
+}
+
+let getGroupStudents = (group) => {
+	let studentsName = []
+	group.forEach(obj => studentsName.push(obj.name))
+	return studentsName;
+}
+
+console.log(getGroup('Orchid'));
+console.log(getGroup('SteelBlue'));
+console.log(getGroup('YellowGreen'));
+console.log(getGroup('IndianRed'));
+console.log(getGroup('LightSeaGreen'));
